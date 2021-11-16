5158ed22de242ad966d3dbb5b677696727956b1c
JeanCarlo96 <jeancarlo.marchesini@yahoo.es>
2021-11-16 13:03:19 -0500

    JCMA Archivos iniciales del proyecto

---
+++ ts-intro/src/index.ts
@@ -0,0 +1,12 @@
+/*
+    ===== Código de TypeScript
+*/
+
+let nombre: string = 'Strider';
+let hp: number | string = 95;
+let estaVivo: boolean = true;
+
+nombre = 'Jean Carlo';
+hp = 'FUll';
+
+console.log('Nombre: ' + nombre + ' Hp: ' + hp + ' Esta vivo: ' + estaVivo)
